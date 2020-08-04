43698e1a777e0fb43b14b5703b851fef99a35796
William Porto <powilliam19@gmail.com>
2020-08-04 18:06:34 -0400

    server: create NestJS application from boilerplate

---
+++ packages/server/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(process.env.APP_PORT);
+  console.log(`> [app]: Listening on *:${process.env.APP_PORT}`);
+}
+
+bootstrap();
